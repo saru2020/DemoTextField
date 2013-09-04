ea153cf12f226263ce4d983525febd6e01eddf66
vimalmurugan89 <vimaliphone@gmail.com>
2013-09-04 10:25:24 +0530

    Appdelegate file

---
+++ AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  DemoTextField
+//
+//  Created by admin on 03/09/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
